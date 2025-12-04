0b8a460eeaba0d842b430c608e747a79f1520cc5
anzboi <daniel.cantos@anz.com>
2025-12-04 17:07:01 +1100

    add go day4 benchmarks

---
+++ go/day4/bench_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+var input = read("../../input/day4/puzzle.txt")
+
+var anchor int
+
+// goos: darwin
+// goarch: arm64
+// pkg: github.com/dancantos/aoc2025/go/day4
+// cpu: Apple M4 Pro
+// BenchmarkPuzzle1-12    	   11412	    105891 ns/op	       0 B/op	       0 allocs/op
+func BenchmarkPuzzle1(b *testing.B) {
+	var result int
+	for b.Loop() {
+		result = puzzle1(input)
+	}
+	anchor = result
+}
+
+// goos: darwin
+// goarch: arm64
+// pkg: github.com/dancantos/aoc2025/go/day4
+// cpu: Apple M4 Pro
+// BenchmarkPuzzle2-12    	     252	   4600431 ns/op	10720623 B/op	    9174 allocs/op
+func BenchmarkPuzzle2(b *testing.B) {
+	var result int
+	for b.Loop() {
+		result = puzzle2(input)
+	}
+	anchor = result
+}
