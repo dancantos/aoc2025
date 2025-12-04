5a0a1ea87384ae878ce1bb717184cc1b2a4a027f
anzboi <daniel.cantos@anz.com>
2025-12-05 09:02:10 +1100

    fix go bench

--- go/day4/bench_test.go
+++ go/day4/bench_test.go
@@ -23,10 +23,13 @@ func BenchmarkPuzzle1(b *testing.B) {
 // goarch: arm64
 // pkg: github.com/dancantos/aoc2025/go/day4
 // cpu: Apple M4 Pro
-// BenchmarkPuzzle2-12    	   24738	     47442 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkPuzzle2-12    	     632	   1800490 ns/op	       8 B/op	       0 allocs/op
 func BenchmarkPuzzle2(b *testing.B) {
 	var result int
 	for b.Loop() {
+		b.StopTimer()
+		input := input.clone()
+		b.StartTimer()
 		result = puzzle2(input)
 	}
 	anchor = result
