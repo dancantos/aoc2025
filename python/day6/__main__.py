2dc88546897e583626251e6dd590c2f721f00abd
anzboi <daniel.cantos@anz.com>
2025-12-06 17:11:38 +1100

    implement line padding and explain the one-liner

--- python/day6/__main__.py
+++ python/day6/__main__.py
@@ -3,9 +3,11 @@ def read(filename):
     with open(filename) as f:
         chars = [ [ c for c in line.strip().split() ] for line in f ]
 
+    # separate rows of numbers from operators
     nums = [ [ int(n) for n in row ] for row in chars[:-1] ]
     ops = chars[-1]
 
+    # transpose
     problems = [ [ nums[i][j] for i in range(len(nums)) ] for j in range(len(nums[0])) ]
     return problems, ops
 
@@ -13,21 +15,32 @@ def read2(filename):
     with open(filename) as f:
         lines = [ line for line in f ]
 
+    # pad lines
+    maxlen = max([ len(l) for l in lines ])
+    lines = [ line+' '*(maxlen-len(line)) for line in lines ]
+
+    # whitespace between digits now matters, so find the starting indices of each problem
     start_pos = []
     for i in range(1, len(lines[-1])):
         if lines[-1][i] != ' ':
             start_pos.append(i)
 
+    # extract the lines for each problem by iterating over the starting positions
     problems = []
     col_start = 0
     for i in range(len(start_pos)):
         col_end = start_pos[i]-1
         problems.append([ line[col_start:col_end] for line in lines[:-1] ])
         col_start = start_pos[i]
-
+    # last line
     problems.append([ line[col_start:-1] for line in lines[:-1] ])
+
+    # for each problem: transpose -> reverse -> to_num gives us the numbers for each problem
+    # explanation: if we treat each problem array as a matrix of characters, then rotating the matrix gives us the numbers we need
+    # the inputs are arrays of strings, so we need to index into them, then reconstruct the results as strings as we go.
     problems = [ [ str_to_num(''.join([ p[i][j] for i in range(len(p)) ]).strip()[::-1]) for j in range(len(p[0])) ] for p in problems ]
 
+    # the last line of operators still need to be processed
     return problems, lines[-1].strip().split()
 
 def str_to_num(s):
